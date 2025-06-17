f62b312313eaa259d30c39b9cf2b299b47caa358
nonetype <objectobject039@gmail.com>
2025-06-17 23:05:04 +0300

    Update and rename setup.py to setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo "Installation begins"
+
+mkdir ~/.local/simplefetch
+mv simplefetch.py ~/.local/simplefetch
+alias simplefetch='python ~/.local/simplefetch'
+
+echo "Completing the installation"

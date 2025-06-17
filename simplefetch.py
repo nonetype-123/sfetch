66656db2243f7b99efe6a95be1b21deb403f9ab3
nonetype <objectobject039@gmail.com>
2025-06-17 21:57:18 +0300

    Update simplefetch.py

--- simplefetch.py
+++ simplefetch.py
@@ -1,1 +1,18 @@
+import platform, subprocess, distro, os
 
+def output(command):
+        output = subprocess.getoutput(command)
+        return output
+
+info = {"username": f"{output('whoami')}@{output('uname -a').split()[1]}",
+        "os": f"{distro.name()}",
+        "host": f"{output('cat /sys/devices/virtual/dmi/id/product_name')}",
+        "kernel": f"{os.uname()[2]}",
+        "uptime": f"{output('uptime').split()[0] + 'up'}",
+        "shell": f"{output('echo $SHELL')}",
+        "terminal": f"{output('echo $TERM')}",
+        }
+
+info_keys = info.keys()
+
+print(info)

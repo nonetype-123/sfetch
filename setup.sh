40e02c7b032818600bc6874b3e33e4a75ca8c0b7
nonetype <objectobject039@gmail.com>
2025-06-17 23:20:06 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
-echo "Installation begins"
-
 mkdir ~/.local/simplefetch
 mv simplefetch.py ~/.local/simplefetch
 echo "alias simplefetch='python ~/.local/simplefetch/simplefetch.py'" >> ~/.bashrc
 source ~/.bashrc
-
-echo "Completing the installation"
+clear

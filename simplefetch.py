a7064e9ccce5c44f986cf3628e2174d31e577970
nonetype <objectobject039@gmail.com>
2025-06-17 22:39:41 +0300

    Update simplefetch.py

--- simplefetch.py
+++ simplefetch.py
@@ -4,6 +4,19 @@ def output(command):
         output = subprocess.getoutput(command)
         return output
 
+def color_text(text, color):
+    colors = {
+	"red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "purple": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m",
+        "reset": "\033[0m"
+    }
+    return f"{colors.get(color, '')}{text}{colors['reset']}"
+
 battery = psutil.sensors_battery()
 memory = psutil.virtual_memory()
 cpu = os.popen("cat /proc/cpuinfo | grep 'model name' | head -1").read()
@@ -12,7 +25,7 @@ info = {"username": f"{output('whoami')}@{output('uname -a').split()[1]}",
         "os": f"{distro.name()}",
         "host": f"{output('cat /sys/devices/virtual/dmi/id/product_name')}",
         "kernel": f"{os.uname()[2]}",
-        "uptime": f"{output('uptime').split()[0] + 'up'}",
+        "uptime": f"{output('uptime').split()[0]} up",
         "shell": f"{output('echo $SHELL')}",
         "terminal": f"{output('echo $TERM')}",
         "language": f"{output('echo $LANG').split('.')[0]}",
@@ -22,6 +35,7 @@ info = {"username": f"{output('whoami')}@{output('uname -a').split()[1]}",
         "battery": f"{'Battery not detected' if battery is None else int(battery.percent)}%"
         }
 
-info_keys = info.keys()
+info_keys = list(info.keys())
 
-print(info)
+for i in range(0, len(info_keys)-1):
+        print(color_text(f"{info_keys[i]}: ", "purple") + f"{info[info_keys[i]]}")

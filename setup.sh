f2702277811f35ec472e56750277941c6ef98d17
nonetype <objectobject039@gmail.com>
2025-06-18 12:55:03 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir /usr/local/simplefetch
 sudo cp simplefetch.py /usr/local/simplefetch
-pip install -r requirements
+pip install -r requirements.txt
 echo "alias simplefetch='python /usr/local/simplefetch/simplefetch.py'" >> ~/.bashrc
 source ~/.bashrc
 clear
